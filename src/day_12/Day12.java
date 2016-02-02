e236ec7bdd4724f343274296e8a8908ba9fd4d8a
satanson8517 <michal.nedbalek@gmail.com>
2016-02-03 00:48:48 +0100

    CHANGE usage of regex in Day 12

--- src/day_12/Day12.java
+++ src/day_12/Day12.java
@@ -10,6 +10,8 @@ import java.nio.file.Files;
 import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -17,39 +19,78 @@ import java.nio.file.Paths;
  */
 public class Day12 implements Day {
 
-	static final String DAY_PATH = "day_12";
-	static final String INPUT_FILE = "input.txt";
+    static final String DAY_PATH = "day_12";
+    static final String INPUT_FILE = "input.txt";
 
-	@Override
-	public void run() throws Exception {
-		Path inputFile = Paths.get(AdventOfCode.VAR_PATH, DAY_PATH, INPUT_FILE).toRealPath(LinkOption.NOFOLLOW_LINKS);
-		try (BufferedReader reader = Files.newBufferedReader(inputFile)) {
+    @Override
+    public void run() throws Exception {
+        Path inputFile = Paths.get(AdventOfCode.VAR_PATH, DAY_PATH, INPUT_FILE).toRealPath(LinkOption.NOFOLLOW_LINKS);
+        try (BufferedReader reader = Files.newBufferedReader(inputFile)) {
 //			System.out.println(count(reader));
-			String jsonString = reader.readLine().replaceAll("\"red\"", "");
-			System.out.println(jsonString.length());
-			
-			StringReader sr = new StringReader(jsonString);
-			System.out.println(count(sr));
-		} catch (Exception ex) {
-			ex.printStackTrace(System.out);
-		}
-	}
-	
-	private static int count(Reader reader) throws IOException {
-		int c, sum = 0;
-		StringBuilder sb = new StringBuilder();
+//			String jsonString = reader.readLine().replaceAll("([{][^\\[{]*\"red\"[^\\[]*[}])", "X");
+            String jsonString = reader.readLine();
+//            System.out.println(jsonString.length());
 
-		while ((c = reader.read()) != -1) {
-			if (c == '-' || (c >= '0' && c <= '9')) {
-				sb.append((char) c);
-			} else {
-				if (sb.length() != 0) {
-					sum += Integer.parseInt(sb.toString());
-					sb = new StringBuilder();
-				}
-			}
-		}
-		return sum;
-	}
+            Pattern p = Pattern.compile("([{][^\\[{]*\"red\"[^\\[]*?[}])");
+            Matcher m = p.matcher(jsonString);
+            int i = 0, sumObjBrace = 0;
+            String subs;
+            char[] subsArr;
+            while (m.find()) {
+                i++;
+                subs = m.group();
+                subsArr = subs.toCharArray();
+                sumObjBrace = 0;
+                for (int j = 0; j < subsArr.length; j++){
+                    if (subsArr[j] == '{'){
+                        sumObjBrace++;
+                    } 
+                    if (subsArr[j] == '}'){
+                        sumObjBrace--;
+                    } 
+                }
+                System.out.println(m.group() + ", " + sumObjBrace);
+            }
+
+            StringReader sr = new StringReader(jsonString);
+            System.out.println(count(sr));
+        } catch (Exception ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+
+    private int redCount(Reader reader) throws IOException {
+        int c, sum = 0;
+        StringBuilder sb = new StringBuilder();
+
+        while ((c = reader.read()) != -1) {
+            if (c == '-' || (c >= '0' && c <= '9')) {
+                sb.append((char) c);
+            } else {
+                if (sb.length() != 0) {
+                    sum += Integer.parseInt(sb.toString());
+                    sb = new StringBuilder();
+                }
+            }
+        }
+        return sum;
+    }
+
+    private int count(Reader reader) throws IOException {
+        int c, sum = 0;
+        StringBuilder sb = new StringBuilder();
+
+        while ((c = reader.read()) != -1) {
+            if (c == '-' || (c >= '0' && c <= '9')) {
+                sb.append((char) c);
+            } else {
+                if (sb.length() != 0) {
+                    sum += Integer.parseInt(sb.toString());
+                    sb = new StringBuilder();
+                }
+            }
+        }
+        return sum;
+    }
 
 }
